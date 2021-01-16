c672e80c2a711297158da5520fed033e64d01e0e
Amit Halder <36619176+amith7319@users.noreply.github.com>
2021-01-17 00:55:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,3 +23,20 @@ source /etc/environment
 apt install apache2 -y
 systemctl start apache2
 systemctl enable apache2
+
+
+#Installing Postgress 13
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+apt update -y
+apt install postgresql-13 postgresql-client-13
+systemctl start postgresql.service
+systemctl enable postgresql.service
+POSTGRESPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+su - postgres
+psql -c "alter user postgres with password '$POSTGRESPASSWORD'"
+echo Postgres user password = $POSTGRESPASSWORD >> /root/password.txt
+
+
+#Installing PgAdmin
+apt install pgadmin4 pgadmin4-apache2 -y
